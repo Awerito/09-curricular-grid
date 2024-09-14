67d927e1d5c5d0c5b6bf3215c06a1dc1f88e0627
Awerito <diego@openlink.cl>
2024-09-14 20:16:19 -0300

    Add: Title and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { Container } from '@mantine/core'
+import { Container, Title, Text } from '@mantine/core'
 import CurricularGrid from './components/CurricularGrid'
 
 function App() {
   return (
     <Container size="xl">
+      <Title align="center" style={{ marginTop: 20 }}>Malla Curricular ICINF 2020</Title>
+
       <CurricularGrid />
+
+      <Text c="dimmed" size="sm">
+        2022 Universidad de Los Lagos. MIT License. Creado por <a href="http://github.com/Awerito">Awerito</a>
+      </Text>
     </Container>
   )
 }
